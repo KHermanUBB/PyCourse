b946e12a47e31883389461ab0ff14f5c30a73d96
KHermanUBB <kherman@ubiobio.cl>
2022-09-16 14:57:26 -0300

    strings ok

---
+++ strings.py
@@ -0,0 +1,9 @@
+# comments section
+
+str = 'Hey I am here'
+
+print(str.find('am'))
+print(str.replace('I am', 'You are'))
+str2 = str.replace('I am', 'You are')
+# checks for occurence of the world 'You' in the str2
+print('You' in str2)
